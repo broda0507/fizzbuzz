a7c6f0fd052a8f90848caca1c5af4df721160380
broda0507 <lukasz.brodaczewski@gmail.com>
2019-05-14 18:50:49 +0200

    fixed )

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if not i % 3 and not i % 5:
-            print('fizzbuzz'
+            print('fizzbuzz')
         elif not i % 3:
             print('fizz')
         elif not i % 5:
